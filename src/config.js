9a2a354e97ed1291c6c23c1110c1b7eaf7cdbe44
Wallace H Wu <wallace_h_wu@wistron.com>
2020-09-30 15:04:03 +0800

    update

---
+++ src/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    dev: {
+        template: {
+            title: 'dev',
+            header: false,
+            footer: false
+        }
+    },
+    build: {
+        template: {
+            title: 'build',
+            header: true,
+            footer: false
+        }
+    }
+}

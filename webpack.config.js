e41967f996b86d49cddeb4a62a234a852afc9606
Wallace H Wu <wallace_h_wu@wistron.com>
2020-10-02 17:44:28 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -13,43 +13,57 @@ module.exports = {
                     'babel-loader'
                 ]
             },
-            {
-                test: /\.(css|scss)$/,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
             // {
-            //     test: /\.(sc|c)ss$/,
-            //     use: ['style-loader', 'css-loader', {
-            //         loader: 'postcss-loader',
-            //         options: {
-            //             plugins: function () {
-            //                 return [
-            //                     require('autoprefixer')({
-            //                         "overrideBrowserslist": [
-            //                             ">0.25%",
-            //                             "not dead"
-            //                         ]
-            //                     })
-            //                 ]
-            //             }
-            //         }
-            //     }, 'sass-loader'],
-            //     exclude: /node_modules/
-            // }
+            //     test: /\.(css|scss)$/,
+            //     exclude: /node_modules/,
+            //     use: [
+            //         'style-loader',
+            //         'css-loader',
+            //         'postcss-loader',
+            //         'sass-loader'
+            //     ]
+            // },
+            {
+                test: /\.(sc|c)ss$/,
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: function () {
+                            return [
+                                require('autoprefixer')({
+                                    "overrideBrowserslist": [
+                                        ">0.25%",
+                                        "not dead"
+                                    ]
+                                })
+                            ]
+                        }
+                    }
+                }, 'sass-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240, //10K
+                            esModule: false,
+                            name: '[name]_[hash:6].[ext]'
+                        }
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
+                loader: 'file-loader'
+            },
         ]
     },
     devServer: {
         port: '8080',
-        quiet: false,
-        inline: true,
-        overlay: false,
-        clientLogLevel: "silent",
         compress: true
     },
     plugins: [

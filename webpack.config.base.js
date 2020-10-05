cd313cbc7bcf1f2884de944d7ed0b6e381bb0dc0
Wallace H Wu <wallace_h_wu@wistron.com>
2020-10-05 22:25:08 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,7 +6,20 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ['babel-loader']
             },
+            {
+                test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240, // size 大於 10K 就轉成使用 asset, 小於的話就轉成 64 base
+                            // esModule: false,
+                            // name: '[name]_[hash:6].[ext]'
+                        }
+                    }
+                ],
+                exclude: /node_modules/
+            }
         ]
-    },
-    plugins: ['base']
+    }
 }
